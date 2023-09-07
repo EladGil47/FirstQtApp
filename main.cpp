1e99c477f6e017ac610e7f556d1093d164ce83be
Elad Gil <87933794+EladGil47@users.noreply.github.com>
2023-09-07 17:37:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,33 @@
 
 #include <QApplication>
 
+#include "groups_manager_menu.hpp"
 #include "kohot.hpp"
 
-int main(int argc, char *argv[])
-{
-    Kohot m_kohot;
-    m_kohot.loadGroups();
-    std::shared_ptr<GroupsCollection> groups_collection = m_kohot.getGroupsCollection();
+int main(int argc, char *argv[]) {
 
+  int ret_val = 0;
+  Kohot kohot;
+  kohot.loadGroups();
+  std::shared_ptr<GroupsCollection> groups_collection = kohot.getGroupsCollection();
+
+  bool isConsoleApp = true;
+  if (isConsoleApp) 
+  {
+    GroupsManagerMenu xxx(groups_collection);
+    xxx.handle();
+  }
+  else 
+  {
     QApplication a(argc, argv);
 
-    MainWindow main_window(nullptr,groups_collection);
-    QObject::connect(&main_window, &QObject::destroyed, [&]() {
-    m_kohot.saveGroups();});
-
-
+    MainWindow main_window(nullptr, groups_collection);
+    QObject::connect(&main_window, &QObject::destroyed,
+                     [&]() { kohot.saveGroups(); });
     main_window.show();
-    
 
-    return a.exec();
+    ret_val = a.exec();
+  }
+
+  return ret_val;
 }
