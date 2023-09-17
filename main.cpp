63b65fae1368e069a0cac38af1ff2d3e351262eb
EladGil47 <87933794+EladGil47@users.noreply.github.com>
2023-09-17 04:12:58 +0300

    comment_console_ui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 
-#include "groups_manager_menu.hpp"
+// #include "groups_manager_menu.hpp"
 #include "kohot.hpp"
 
 #include "windows_manager.h"
@@ -17,8 +17,8 @@ int main(int argc, char *argv[]) {
   bool isConsoleApp = false;
   if (isConsoleApp) 
   {
-    GroupsManagerMenu groups_manager_menu(groups_collection);
-    ret_val = groups_manager_menu.handle() ? EXIT_SUCCESS : EXIT_FAILURE;
+    // GroupsManagerMenu groups_manager_menu(groups_collection);
+    // ret_val = groups_manager_menu.handle() ? EXIT_SUCCESS : EXIT_FAILURE;
   }
   else 
   {
