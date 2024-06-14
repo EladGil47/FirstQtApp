07f7604773c1eaaec57057953570b8f42ed19a1b
EladGil47 <87933794+EladGil47@users.noreply.github.com>
2024-06-14 17:18:05 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <QApplication>
 
-#include "kohot.hpp"
 #include "windows_manager.h"
+#include "kohot.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  int ret_val = EXIT_SUCCESS;
-  Kohot kohot;
-  kohot.loadGroups();
-  std::shared_ptr<GroupsCollection> groups_collection = kohot.getGroupsCollection();
+    int   ret_val = EXIT_SUCCESS;
+    Kohot kohot;
+    kohot.loadGroups();
+    std::shared_ptr<GroupsCollection> groups_collection = kohot.getGroupsCollection();
 
-  QApplication a(argc, argv);
-  WindowsManager window(groups_collection);
-  QObject::connect(&window, &QObject::destroyed, [&kohot]()
-                   { kohot.saveGroups(); });
+    QApplication   a(argc, argv);
+    WindowsManager window(groups_collection);
+    QObject::connect(&window, &QObject::destroyed, [&kohot]() { kohot.saveGroups(); });
 
-  ret_val = a.exec();
+    ret_val = a.exec();
 
-  return ret_val;
+    return ret_val;
 }
