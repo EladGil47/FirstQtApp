d27a0c1d3e34c05c583b49de5a065b2d3fdad6bb
elad <eladg@foresightauto.com>
2024-08-16 23:17:33 +0300

    ADD IMAGES

--- include/resource_paths.hpp
+++ include/resource_paths.hpp
@@ -3,7 +3,7 @@
 
 namespace resources_path
 {
-const char* const SOCCER_BALL = ":/images/soccer_ball.png";
+const char* const SOCCER_BALL = "./images/soccer_ball.png";
 } // namespace resources_path
 
 #endif // RESOURCE_PATHS_HPP
