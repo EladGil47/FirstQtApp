039bfe09df6f4efd956857ea45d09c1d0c34e13e
elad <eladg@foresightauto.com>
2024-08-24 16:07:27 +0300

    fix group input dialog

--- include/dialogs/group_input_dialog.hpp
+++ include/dialogs/group_input_dialog.hpp
@@ -11,6 +11,7 @@
 class GroupInputDialog : public QDialog
 {
     Q_OBJECT
+
 public:
     GroupInputDialog(QWidget* parent = nullptr);
     QString getGroupName() const;
@@ -18,11 +19,16 @@ public:
     int32_t getPlayersInTeamAmount() const;
 
 private:
-    std::shared_ptr<QLineEdit>        m_name_line_edit;
-    std::shared_ptr<QSpinBox>         m_players_in_team_spin_box;
-    std::shared_ptr<QSpinBox>         m_teams_spin_box;
-    std::shared_ptr<QDialogButtonBox> m_button_box;
-    std::shared_ptr<QFormLayout>      m_layout;
+    std::unique_ptr<QLineEdit>        m_name_line_edit;
+    std::unique_ptr<QSpinBox>         m_players_in_team_spin_box;
+    std::unique_ptr<QSpinBox>         m_teams_spin_box;
+    std::unique_ptr<QDialogButtonBox> m_button_box;
+    std::unique_ptr<QFormLayout>      m_layout;
+
+    bool validateInput() const;
+
+private slots:
+    void onAccepted();
 };
 
 #endif // FIRST_QT_APP_INCLUDE_GROUP_INPUT_DIALOG_HPP
--- src/dialogs/group_input_dialog.cpp
+++ src/dialogs/group_input_dialog.cpp
@@ -3,40 +3,39 @@
 #include "resource_paths.hpp"
 #include "settings.hpp"
 
-namespace
-{
+#include <QMessageBox>
+
 static constexpr const char* WINDOW_TITLE          = "Create new group";
 static constexpr const char* LABEL_NAME            = "Name:";
 static constexpr const char* LABEL_TEAMS           = "Teams:";
 static constexpr const char* LABEL_PLAYERS_IN_TEAM = "Players in team:";
-} // namespace
 
 GroupInputDialog::GroupInputDialog(QWidget* parent)
     : QDialog(parent)
 {
     setWindowTitle(WINDOW_TITLE);
 
-    m_name_line_edit = std::make_shared<QLineEdit>(this);
+    m_name_line_edit = std::make_unique<QLineEdit>(this);
 
-    m_teams_spin_box = std::make_shared<QSpinBox>(this);
-    int32_t teamsMin = Settings::getTeamsRange()[0];
-    int32_t teamsMax = Settings::getTeamsRange()[1];
-    m_teams_spin_box->setRange(teamsMin, teamsMax);
+    m_teams_spin_box = std::make_unique<QSpinBox>(this);
+    m_teams_spin_box->setRange(
+        Settings::getTeamsRange()[0],
+        Settings::getTeamsRange()[1]);
 
-    m_players_in_team_spin_box = std::make_shared<QSpinBox>(this);
-    int32_t playersMin         = Settings::getPlayersInTeamRange()[0];
-    int32_t playersMax         = Settings::getPlayersInTeamRange()[1];
-    m_players_in_team_spin_box->setRange(playersMin, playersMax);
+    m_players_in_team_spin_box = std::make_unique<QSpinBox>(this);
+    m_players_in_team_spin_box->setRange(
+        Settings::getPlayersInTeamRange()[0],
+        Settings::getPlayersInTeamRange()[1]);
 
-    m_button_box = std::make_shared<QDialogButtonBox>(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    m_button_box = std::make_unique<QDialogButtonBox>(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
 
-    m_layout = std::make_shared<QFormLayout>(this);
+    m_layout = std::make_unique<QFormLayout>(this);
     m_layout->addRow(LABEL_NAME, m_name_line_edit.get());
     m_layout->addRow(LABEL_TEAMS, m_teams_spin_box.get());
     m_layout->addRow(LABEL_PLAYERS_IN_TEAM, m_players_in_team_spin_box.get());
     m_layout->addWidget(m_button_box.get());
 
-    connect(m_button_box.get(), &QDialogButtonBox::accepted, this, &GroupInputDialog::accept);
+    connect(m_button_box.get(), &QDialogButtonBox::accepted, this, &GroupInputDialog::onAccepted);
     connect(m_button_box.get(), &QDialogButtonBox::rejected, this, &GroupInputDialog::reject);
 }
 
@@ -53,4 +52,22 @@ int32_t GroupInputDialog::getTeamsAmount() const
 int32_t GroupInputDialog::getPlayersInTeamAmount() const
 {
     return m_players_in_team_spin_box->value();
+}
+
+void GroupInputDialog::onAccepted()
+{
+    if (validateInput())
+    {
+        accept();
+    }
+    else
+    {
+        static constexpr const char* ERROR_MESSAGE = "Name cannot be empty.";
+        QMessageBox::warning(this, WINDOW_TITLE, ERROR_MESSAGE);
+    }
+}
+
+bool GroupInputDialog::validateInput() const
+{
+    return !m_name_line_edit->text().isEmpty();
 }
