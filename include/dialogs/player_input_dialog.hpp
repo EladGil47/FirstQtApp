ae727ad7cf404759a2c00146e11c86ddbdcb3cc7
elad <eladg@foresightauto.com>
2024-08-24 16:22:18 +0300

    fix player input dialog

--- include/dialogs/player_input_dialog.hpp
+++ include/dialogs/player_input_dialog.hpp
@@ -1,62 +1,30 @@
 #ifndef FIRST_QT_APP_INCLUDE_PLAYER_INPUT_DIALOG_HPP
 #define FIRST_QT_APP_INCLUDE_PLAYER_INPUT_DIALOG_HPP
 
-#include <QDialog>
-#include <QFormLayout>
-#include <QLineEdit>
-#include <QDoubleSpinBox>
 #include <QComboBox>
-#include <QDialogButtonBox>
+#include <QDialog>
+#include <QDoubleSpinBox>
+#include <QLineEdit>
+#include <memory>
 
 class PlayerInputDialog : public QDialog
 {
     Q_OBJECT
 public:
-    PlayerInputDialog(QWidget *parent = nullptr) : QDialog(parent)
-    {
-        setWindowTitle("Create new player");
+    explicit PlayerInputDialog(QWidget* parent = nullptr);
 
-        m_name_line_edit = new QLineEdit(this);
-
-        m_rate_spin_box = new QDoubleSpinBox(this);
-        m_rate_spin_box->setRange(0, 7);
-        m_rate_spin_box->setSingleStep(1);
-
-        m_role_combo_box = new QComboBox(this);
-        m_role_combo_box->addItem("Baller");
-        m_role_combo_box->addItem("GK");
-
-        QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
-        connect(buttonBox, &QDialogButtonBox::accepted, this, &PlayerInputDialog::accept);
-        connect(buttonBox, &QDialogButtonBox::rejected, this, &PlayerInputDialog::reject);
-
-        QFormLayout *form_layout = new QFormLayout(this);
-        form_layout->addRow("Name:", m_name_line_edit);
-        form_layout->addRow("Rate:", m_rate_spin_box);
-        form_layout->addRow("Role:", m_role_combo_box);
-
-        form_layout->addWidget(buttonBox);
-    }
-
-    QString getPlayerName() const
-    {
-        return m_name_line_edit->text();
-    }
-
-    QString getPlayerRole() const
-    {
-        return m_role_combo_box->currentText();
-    }
-
-    double getPlayerRate() const
-    {
-        return m_rate_spin_box->value();
-    }
+    QString getPlayerName() const;
+    QString getPlayerRole() const;
+    double  getPlayerRate() const;
 
 private:
-    QLineEdit *m_name_line_edit;
-    QDoubleSpinBox *m_rate_spin_box;
-    QComboBox *m_role_combo_box;
+    std::unique_ptr<QLineEdit>      m_name_line_edit;
+    std::unique_ptr<QDoubleSpinBox> m_rate_spin_box;
+    std::unique_ptr<QComboBox>      m_role_combo_box;
+    bool                            validateInput() const;
+
+private slots:
+    void onAccepted();
 };
 
 #endif // FIRST_QT_APP_INCLUDE_PLAYER_INPUT_DIALOG_HPP
---
+++ src/dialogs/player_input_dialog.cpp
@@ -0,0 +1,71 @@
+#include "player_input_dialog.hpp"
+#include <QDialogButtonBox>
+#include <QFormLayout>
+
+#include <QMessageBox>
+
+constexpr const char* WINDOW_TITLE = "Create new player";
+constexpr const char* NAME_LABEL   = "Name:";
+constexpr const char* RATE_LABEL   = "Rate:";
+constexpr const char* ROLE_LABEL   = "Role:";
+constexpr const char* BALLER_LABEL = "Baller";
+constexpr const char* GK_LABEL     = "GK";
+
+PlayerInputDialog::PlayerInputDialog(QWidget* parent)
+    : QDialog(parent),
+      m_name_line_edit(std::make_unique<QLineEdit>(this)),
+      m_rate_spin_box(std::make_unique<QDoubleSpinBox>(this)),
+      m_role_combo_box(std::make_unique<QComboBox>(this))
+{
+    setWindowTitle(WINDOW_TITLE);
+
+    m_rate_spin_box->setRange(0, 7);
+    m_rate_spin_box->setSingleStep(1);
+
+    m_role_combo_box->addItem(BALLER_LABEL);
+    m_role_combo_box->addItem(GK_LABEL);
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &PlayerInputDialog::onAccepted);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &PlayerInputDialog::reject);
+
+    QFormLayout* form_layout = new QFormLayout(this);
+    form_layout->addRow(NAME_LABEL, m_name_line_edit.get());
+    form_layout->addRow(RATE_LABEL, m_rate_spin_box.get());
+    form_layout->addRow(ROLE_LABEL, m_role_combo_box.get());
+
+    form_layout->addWidget(buttonBox);
+}
+
+QString PlayerInputDialog::getPlayerName() const
+{
+    return m_name_line_edit->text();
+}
+
+QString PlayerInputDialog::getPlayerRole() const
+{
+    return m_role_combo_box->currentText();
+}
+
+double PlayerInputDialog::getPlayerRate() const
+{
+    return m_rate_spin_box->value();
+}
+
+void PlayerInputDialog::onAccepted()
+{
+    if (validateInput())
+    {
+        accept();
+    }
+    else
+    {
+        static constexpr const char* ERROR_MESSAGE = "Name cannot be empty.";
+        QMessageBox::warning(this, WINDOW_TITLE, ERROR_MESSAGE);
+    }
+}
+
+bool PlayerInputDialog::validateInput() const
+{
+    return !m_name_line_edit->text().isEmpty();
+}
